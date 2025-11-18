0af800a5c2bb739826027c951f0d7c8d53abc684
mgamboa003-hue <mgamboa003@gmail.com>
2025-11-18 13:43:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def aplicar_filtros(df, maquina=None, responsable=None, fecha_desde=None, fecha_
 
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 app.secret_key = 'wintec_secret_key'
 load_dotenv()
 
